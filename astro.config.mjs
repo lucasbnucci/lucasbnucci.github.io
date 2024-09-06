fdbf4a7410221437cf564d44a41626e1e3b4fcde
Lucas B. Nucci <lucasbnucci@gmail.com>
2024-09-05 23:43:55 -0300

    trying to fix actions error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import tailwind from '@astrojs/tailwind'
 
-const LIVE_URL = 'https://lucasbnucci.github.io'
-
 // https://astro.build/config
+
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://lucasbnucci.github.io',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
         shikiConfig: {
